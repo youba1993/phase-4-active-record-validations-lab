2928a7816e8b4eee2b15efd0f8ec6ddc3a115e2e
youbaidjis <youba.etud@gmail.com>
2022-08-24 21:21:30 -0700

    commit tree bc5c443e8cc9e9e1e3725ead76a608511813683f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction' , 'Non-Fiction']}
+    validate :clickbait?
+
+    #seen in the solution branch
+    CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+    def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
